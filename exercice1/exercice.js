cdffa1e98936c3e6ac4c3ef42f1fa36e3548a0cb
Sylvie-B <unsolo@hotmail.fr>
2021-12-22 13:52:12 +0100

    animate right

--- exercice1/exercice.js
+++ exercice1/exercice.js
@@ -1,4 +1,10 @@
-$("div").animate({
-    left: "250px",
-    height: "500px"
+let div = $('div');
+
+// div.animate({
+//     left: "250px",
+//     height: "500px"
+// });
+
+div.animate({
+    right: "250px"
 });
